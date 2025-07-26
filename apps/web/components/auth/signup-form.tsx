f35cd637035c43da4f1121fd863ab54f01dc8fb4
heronrush <isonade.git@gmail.com>
2025-07-26 08:53:10 +0530

    signup form functionality is working

--- apps/web/components/auth/signup-form.tsx
+++ apps/web/components/auth/signup-form.tsx
@@ -1,11 +1,35 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import React, { useState } from "react";
+import axios from "axios";
+import { redirect } from "next/navigation";
 
 export function SignupForm({ className, ...props }: React.ComponentProps<"div">) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    const response = await axios.post("http://localhost:3002/api/signup", {
+      email: email,
+      password: password,
+    });
+
+    if (response.data.msg === "signup successful") {
+      redirect("/user/dashboard");
+    } else {
+      return;
+    }
+  }
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -14,24 +38,49 @@ export function SignupForm({ className, ...props }: React.ComponentProps<"div">)
           <CardDescription>Signup to create short URL</CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="grid gap-6">
               <div className="grid gap-6">
                 <div className="grid gap-3">
                   <Label htmlFor="email">Email</Label>
-                  <Input id="email" type="email" placeholder="johndoe@example.com" required />
+                  <Input
+                    id="email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                    placeholder="johndoe@example.com"
+                    required
+                  />
                 </div>
                 <div className="grid gap-3">
                   <div className="flex items-center">
                     <Label htmlFor="password">Password</Label>
                   </div>
-                  <Input id="password" type="password" required />
+                  <Input
+                    id="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    required
+                  />
                 </div>
                 <div className="grid gap-3">
                   <div className="flex items-center">
-                    <Label htmlFor="password">Confirm password</Label>
+                    <Label htmlFor="confirm_password">Confirm password</Label>
                   </div>
-                  <Input id="password" type="password" required />
+                  <Input
+                    id="confirm_password"
+                    type="password"
+                    value={confirmPassword}
+                    onChange={(e) => {
+                      setConfirmPassword(e.target.value);
+                    }}
+                    required
+                  />
                 </div>
                 <Button type="submit" className="w-full cursor-pointer">
                   Signup
